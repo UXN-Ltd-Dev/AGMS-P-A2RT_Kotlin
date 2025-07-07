290219003e21588a53e4dd555f5deddf5787b348
choco5732 <choco5732@gmail.com>
2025-07-07 14:24:47 +0900

    0.3으로 버전수정, 측정시간 3일로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 6
-        versionName = "0.3.0"
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
