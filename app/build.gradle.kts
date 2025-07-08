7312716202e266217c5df08e2eb5aa07060f1089
choco5732 <choco5732@gmail.com>
2025-07-08 18:12:47 +0900

    0.3.2, 14일 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 9
-        versionName = "0.3.1(BLE)"
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- app/src/main/java/kr/co/uxn/agms_p/ui/components/ready/StabilizationScreen.kt
+++ app/src/main/java/kr/co/uxn/agms_p/ui/components/ready/StabilizationScreen.kt
@@ -169,8 +169,8 @@ fun StabilizationScreen(navController: NavController, mac: String, bleViewModel:
 
                 // 2. 측정 기간 설정
     //        val measurementTime: Long = 1000 * 60 * 60 * 24 * 10 // 측정 기간 10일
-//            val measurementTime: Long = 1000 * 60 * 60 * 24 * 14 // 측정 기간 14일
-                val measurementTime: Long = 1000 * 60 * 60 * 24 * 3// 측정 기간 3일
+            val measurementTime: Long = 1000 * 60 * 60 * 24 * 14 // 측정 기간 14일
+//                val measurementTime: Long = 1000 * 60 * 60 * 24 * 3// 측정 기간 3일
 //                val measurementTime: Long = 1000 * 60 * 20 // 측정 시간 20분
 //                val measurementTime: Long = 1000 * 60 * 30 // 측정 시간 30분
 
