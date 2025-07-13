1f0b917692904ff3d27cfeb7373d197dcacb1ff6
choco5732 <choco5732@gmail.com>
2025-07-13 11:26:09 +0900

    okhttp http logging 인터셉터 주석

--- app/src/main/java/kr/co/uxn/agms_p/api/RetrofitClient.kt
+++ app/src/main/java/kr/co/uxn/agms_p/api/RetrofitClient.kt
@@ -14,11 +14,7 @@ object RetrofitClient {
 
     private val okHttpClient by lazy {
         OkHttpClient.Builder()
-            .addInterceptor(
-                HttpLoggingInterceptor(logger = HttpLoggingInterceptor.Logger.DEFAULT).apply {
-                    level = HttpLoggingInterceptor.Level.BODY
-                }
-            )
+//            .addInterceptor(HttpLoggingInterceptor(logger = HttpLoggingInterceptor.Logger.DEFAULT).apply { level = HttpLoggingInterceptor.Level.BODY })
             .build()
     }
 
@@ -34,11 +30,7 @@ object RetrofitClient {
 
     private val okHttpClient2 by lazy {
         OkHttpClient.Builder()
-            .addInterceptor(
-                HttpLoggingInterceptor(logger = HttpLoggingInterceptor.Logger.DEFAULT).apply {
-                    level = HttpLoggingInterceptor.Level.BODY
-                }
-            )
+//            .addInterceptor(HttpLoggingInterceptor(logger = HttpLoggingInterceptor.Logger.DEFAULT).apply { level = HttpLoggingInterceptor.Level.BODY })
             .addInterceptor(TokenInterceptor())
             .authenticator(TokenAuthenticator())
             .build()
@@ -55,11 +47,7 @@ object RetrofitClient {
 
     private val okHttpClient3 by lazy {
         OkHttpClient.Builder()
-            .addInterceptor(
-                HttpLoggingInterceptor(logger = HttpLoggingInterceptor.Logger.DEFAULT).apply {
-                    level = HttpLoggingInterceptor.Level.BODY
-                }
-            )
+//            .addInterceptor(HttpLoggingInterceptor(logger = HttpLoggingInterceptor.Logger.DEFAULT).apply { level = HttpLoggingInterceptor.Level.BODY })
             .build()
     }
 
