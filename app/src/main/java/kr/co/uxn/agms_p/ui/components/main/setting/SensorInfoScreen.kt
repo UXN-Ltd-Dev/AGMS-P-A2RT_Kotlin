4b7841b1a26a2d51e4f955fdf8dee65e0bd301af
choco5732 <choco5732@gmail.com>
2025-07-16 08:24:21 +0900

    설정화면의 세부화면에도 다이얼로그 적용

--- app/src/main/java/kr/co/uxn/agms_p/ui/components/main/setting/SensorInfoScreen.kt
+++ app/src/main/java/kr/co/uxn/agms_p/ui/components/main/setting/SensorInfoScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -38,6 +39,7 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.NavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -45,8 +47,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kr.co.uxn.agms_p.api.RetrofitClient.tokenRetrofit
 import kr.co.uxn.agms_p.api.token.DataStoreManager
+import kr.co.uxn.agms_p.ble.BleBridge
+import kr.co.uxn.agms_p.ble.BleBridge.showHighGlucoseDialog
+import kr.co.uxn.agms_p.ble.BleBridge.showLowGlucoseDialog
 import kr.co.uxn.agms_p.room.AppDatabase
 import kr.co.uxn.agms_p.ui.components.main.AlwaysDialog
+import kr.co.uxn.agms_p.ui.components.main.NotiDialog
 import kr.co.uxn.agms_p.ui.viewmodel.BleViewModel
 import java.time.Instant
 import java.time.format.DateTimeFormatter
@@ -67,6 +73,15 @@ fun SensorInfoScreen(navController: NavController, bleViewModel: BleViewModel) {
     val remainingTime = remember { mutableStateOf(-1) }
 
     var serialNumber = remember { mutableStateOf("")}
+
+    // 다이얼로그 변수 모음
+    var showCaliDialog = bleViewModel.showCaliDialog.collectAsState()
+    var showBleConnectDialog = bleViewModel.showBleConnectDialog.collectAsState()
+    var showBluetoothOnDialog = bleViewModel.showBluetoothOnDialog.collectAsState()
+    var showLowGlucoseDialog = bleViewModel.showLowGlucoseDialog.collectAsState()
+    var showHighGlucoseDialog = bleViewModel.showHighGlucoseDialog.collectAsState()
+    var showEndMeasurementDialog = bleViewModel.showEndMeasurementDialog.collectAsState()
+
 
     val localDbRepository by lazy {
         AppDatabase.getInstance(context)
@@ -169,6 +184,133 @@ fun SensorInfoScreen(navController: NavController, bleViewModel: BleViewModel) {
 //        )
 //    }
 
+    // 다이얼로그
+    // 1. Dialog : 일일 혈당 입력
+    if (showCaliDialog.value) {
+        NotiDialog(
+            onDismiss = { BleBridge.showCaliDialog(false) },
+            onConfirm = {
+                BleBridge.showCaliDialog(false)
+                navController.navigate("GlucoseRegisterScreen")
+            },
+            title = "혈당 입력 시간입니다.",
+            content = "정확한 측정을 위해 공복 상태에서 자가 채혈한 혈당값을 입력해주세요.",
+        )
+    }
+
+    // 2. Dialog : BLE 끊김
+    if (showBleConnectDialog.value) {
+        NotiDialog(
+            onDismiss = { BleBridge.showBleConnectDialog(false) },
+            onConfirm = {
+                BleBridge.showBleConnectDialog(false)
+            },
+            title = "블루투스 연결이 끊어졌습니다.",
+            content = "센서와의 연결이 일시적으로 끊어졌어요.\n스마트폰을 가까이 두고 연결 상태를 확인하세요.",
+        )
+    }
+
+    // 4. Dialog : 블루투스 ON
+    if (showBluetoothOnDialog.value) {
+        NotiDialog(
+            onDismiss = { BleBridge.showBluetoothOnDialog(false) },
+            onConfirm = {
+                BleBridge.showBluetoothOnDialog(false)
+            },
+            title = "블루투스가 꺼져있습니다.",
+            content = "블루투스를 켜고 연결 상태를 확인하세요.",
+        )
+    }
+
+    // 5. Dialog : 측정 종료
+    if (showEndMeasurementDialog.value) {
+        NotiDialog(
+            onDismiss = { BleBridge.showBleConnectDialog(false) },
+            onConfirm = {
+                BleBridge.showBleConnectDialog(false)
+                coroutineScope.launch(Dispatchers.IO) {
+                    val userId = DataStoreManager.getUserId().first() ?: -1
+                    try {
+                        val sensorOff = tokenRetrofit.doSensorOff(userId)
+                        if (sensorOff.isSuccessful) {
+                            val sensorOffBody = sensorOff.body()
+                            if (sensorOffBody != null) {
+                                Log.w("TEST", "sensorOff responseBody : ${sensorOffBody}")
+                                if (sensorOffBody.isSuccess) {
+                                    // userId의 db삭제
+                                    localDbRepository?.dataDao()?.deleteUserValueTable(userId)
+                                    localDbRepository?.dataDao()?.deleteUserGlucoseTable(userId)
+                                    localDbRepository?.dataDao()?.deleteUserCalibrationTable(userId)
+
+                                    Log.w("TEST", "sensorOff 성공")
+                                    DataStoreManager.saveIsMain(false)
+                                    DataStoreManager.deleteRoute()
+                                    DataStoreManager.saveRoute("Splash")
+                                    Log.d("TEST", "${DataStoreManager.getIsMain().first()}")
+                                    DataStoreManager.deleteAccessToken()
+                                    DataStoreManager.deleteRefreshToken()
+                                    DataStoreManager.deleteUserId()
+                                    DataStoreManager.deleteDeviceMac()
+                                    DataStoreManager.deleteStartTime()
+                                    DataStoreManager.deleteEndTime()
+                                    DataStoreManager.deleteDailyCalibrationTime()
+                                    DataStoreManager.deleteDailyCalibrationLastTime()
+                                    DataStoreManager.setLandScapeMode(false)
+                                    DataStoreManager.deleteTargetLowGlucose()
+                                    DataStoreManager.deleteTargetHighGlucose()
+                                    DataStoreManager.deleteEmail()
+                                    withContext(Dispatchers.Main) {
+                                        // 1. 서비스 종료
+                                        bleViewModel.emit("STOP_SERVICE")
+                                        // 앱 강제 종료
+                                        android.os.Process.killProcess(android.os.Process.myPid())
+                                        exitProcess(0)
+                                    }
+                                } else {
+                                    Log.w("TEST", "sensorOff 실패")
+                                }
+                            }
+                        } else {
+                            Log.w("TEST", "sensorOff API통신 실패 : ${sensorOff.errorBody()?.string()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d("TEST", "sensorOff API통신 실패 : ${e.message}")
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(context, "네트워크를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            },
+            title = "센서의 사용 기간이 종료되었습니다.",
+            content = "센서의 사용 기간이 만료되어 더 이상 측정이 불가합니다. 새 센서를 연결해주세요.",
+        )
+    }
+
+    // 6.1 Dialog : 저혈당
+    if (showLowGlucoseDialog.value) {
+        NotiDialog(
+            onDismiss = { showLowGlucoseDialog(false) },
+            onConfirm = {
+                showLowGlucoseDialog(false)
+                NotificationManagerCompat.from(context).cancel(95)
+            },
+            title = "혈당수치가 낮습니다.",
+            content = "저혈당 위험이 있어요. 필요시 조치를 취하고, 안정 후 수치를 다시 확인하세요.",
+        )
+    }
+
+    // 6.2 Dialog : 고혈당
+    if (showHighGlucoseDialog.value) {
+        NotiDialog(
+            onDismiss = { showHighGlucoseDialog(false) },
+            onConfirm = {
+                showHighGlucoseDialog(false)
+                NotificationManagerCompat.from(context).cancel(96)
+            },
+            title = "혈당수치가 높습니다.",
+            content = "현재 혈당이 혈당 범위를 초과했어요. 식사나 활동 내용을 확인하세요.",
+        )
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
