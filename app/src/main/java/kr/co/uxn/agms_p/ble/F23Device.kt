4273ec37ecd20985a23da8753ae011d44bfdb092
choco5732 <choco5732@gmail.com>
2026-01-21 16:13:04 +0900

    F23 프로토콜 전류, 온도 주소 주석처리

--- app/src/main/java/kr/co/uxn/agms_p/ble/F23Device.kt
+++ app/src/main/java/kr/co/uxn/agms_p/ble/F23Device.kt
@@ -285,15 +285,15 @@ class F23Device() : Protocol {
             for (i in 0 until nDataLength) {
                 val nTHigh: Int = 16 + i * step
                 val nTLow: Int = 17 + i * step
-                Log.e(TEST, "TEMP : $nTHigh : $nTLow")
+//                Log.e(TEST, "TEMP : $nTHigh : $nTLow")
                 val nWHigh: Int = 18 + i * step
                 val nWMiddle: Int = 19 + i * step
                 val nWLow: Int = 20 + i * step
-                Log.e(TEST, "WE Address : $nWHigh : $nWMiddle : $nWLow")
+//                Log.e(TEST, "WE Address : $nWHigh : $nWMiddle : $nWLow")
                 val nAHigh: Int = 21 + i * step
                 val nAMiddle: Int = 22 + i * step
                 val nALow: Int = 23 + i * step
-                Log.e(TEST, "AE Address : " + nAHigh + " : " + nAMiddle + " : " + nALow)
+//                Log.e(TEST, "AE Address : " + nAHigh + " : " + nAMiddle + " : " + nALow)
 
                 val time: Long = startTime + (1000 * 10 * i)
 
