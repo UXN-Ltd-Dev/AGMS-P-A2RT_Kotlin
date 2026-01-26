93c0c5d6f4d6a30b4142ed4043021b9b98ab9dee
choco5732 <choco5732@gmail.com>
2026-01-26 13:15:23 +0900

    무결성 검증 통과! withContext Main 으로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "kr.co.uxn.agms_p"
         minSdk = 29
         targetSdk = 35
-        versionCode = 35
+        versionCode = 38
         versionName = "0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
