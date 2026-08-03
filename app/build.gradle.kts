d5a589520fb361856b729f141c97f3515fb9365c
choco5732 <choco5732@gmail.com>
2026-08-03 14:38:06 +0900

    버전코드 2 상향

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "kr.co.uxn.agms_p_a2rt"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
